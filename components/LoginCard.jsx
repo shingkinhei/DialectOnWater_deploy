7af1c9bea980b1539acb8030c06b61756f5394f2
Lauris Kwan <kwanlauris@gmail.com>
2023-03-17 03:36:34 +0800

    created sign in page

--- components/LoginCard.jsx
+++ components/LoginCard.jsx
@@ -1,50 +1,57 @@
 import {
   Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
   Input,
   Checkbox,
   Button,
+  Typography,
 } from "@material-tailwind/react";
 
-export default function LoginCard() {
+export default function Example() {
   return (
-    <Card className="w-96">
-      <CardHeader
-        variant="gradient"
-        color="blue"
-        className="mb-4 grid h-28 place-items-center"
-      >
-        <Typography variant="h3" color="white">
-          Sign In
-        </Typography>
-      </CardHeader>
-      <CardBody className="flex flex-col gap-4">
-        <Input label="Email" size="lg" />
-        <Input label="Password" size="lg" />
-        <div className="-ml-2.5">
-          <Checkbox label="Remember Me" />
+    <Card color="transparent" shadow={false}>
+      <Typography variant="h4" color="blue-gray">
+        Sign Up
+      </Typography>
+      <Typography color="gray" className="mt-1 font-normal">
+        Enter your details to register.
+      </Typography>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-4 flex flex-col gap-6">
+          <Input size="lg" label="Name" />
+          <Input size="lg" label="Email" />
+          <Input type="password" size="lg" label="Password" />
         </div>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button variant="gradient" fullWidth>
-          Sign In
+        <Checkbox
+          label={
+            <Typography
+              variant="small"
+              color="gray"
+              className="flex items-center font-normal"
+            >
+              I agree the
+              <a
+                href="#"
+                className="font-medium transition-colors hover:text-blue-500"
+              >
+                &nbsp;Terms and Conditions
+              </a>
+            </Typography>
+          }
+          containerProps={{ className: "-ml-2.5" }}
+        />
+        <Button className="mt-6" fullWidth>
+          Register
         </Button>
-        <Typography variant="small" className="mt-6 flex justify-center">
-          Don't have an account?
-          <Typography
-            as="a"
-            href="#signup"
-            variant="small"
-            color="blue"
-            className="ml-1 font-bold"
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          Already have an account?{" "}
+          <a
+            href="#"
+            className="font-medium text-blue-500 transition-colors hover:text-blue-700"
           >
-            Sign up
-          </Typography>
+            Sign In
+          </a>
         </Typography>
-      </CardFooter>
+      </form>
     </Card>
   );
 }
