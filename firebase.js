5c65c54403e982561b14be8b24e28b8afee7b362
Lauris Kwan <kwanlauris@gmail.com>
2023-03-16 16:34:07 +0800

    firebase configuration, initial login card

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkYeShat1ABN68PpPCdpqjU41UH57YBrA",
+  authDomain: "dow-development.firebaseapp.com",
+  projectId: "dow-development",
+  storageBucket: "dow-development.appspot.com",
+  messagingSenderId: "646719160270",
+  appId: "1:646719160270:web:1bf46a47b633302ede759f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
