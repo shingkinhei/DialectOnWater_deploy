5a98ba00e6b4c4948825bb39762e43870bf12ed8
Lauris Kwan <kwanlauris@gmail.com>
2023-03-19 22:46:18 +0800

    sign in form v2

--- contexts/AuthContext.jsx
+++ contexts/AuthContext.jsx
@@ -1,6 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import { auth, db } from "@/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 
 const AuthContext = React.createContext();
@@ -10,8 +14,21 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(true); // prevent flickering by auth when refreshing page.
   const [currentUser, setCurrentUser] = useState();
   const [signUpMessage, setSignUpMessage] = useState("");
+  const [signInMessage, setSignInMessage] = useState("");
+
+  async function logOut() {
+    return await signOut(auth)
+      .then(() => {
+        setCurrentUser(null);
+        console.log("logged out");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   async function signup(email, password, displayName) {
     await createUserWithEmailAndPassword(auth, email, password)
@@ -29,22 +46,42 @@ export function AuthProvider({ children }) {
       });
   }
 
+  async function localSignIn(email, password) {
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log("logged In");
+      })
+      .catch((error) => {
+        setSignInMessage(error.message);
+      });
+  }
+
   useEffect(() => {
-    const unsubsribe = auth.onAuthStateChanged((user) => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setCurrentUser(user);
+        console.log(user);
       }
+      setLoading(false);
     });
 
-    return unsubsribe;
+    return unsubscribe;
   }, []);
 
   const value = {
     currentUser,
+    logOut,
     signup,
     signUpMessage,
     setSignUpMessage,
+    signInMessage,
+    setSignInMessage,
+    localSignIn,
   };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
