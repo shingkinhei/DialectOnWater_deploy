3a45b951981fc4d45553f1b38ba5c24e620ad6c8
shingkinhei <82117981+shingkinhei@users.noreply.github.com>
2023-04-07 02:47:35 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -15,9 +15,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-firebase.initializeApp(firebaseConfig);
 const db = getFirestore();
 
-export default firebase;
 export const auth = getAuth(app);
 export { app, db };
