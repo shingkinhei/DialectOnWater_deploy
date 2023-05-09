37a116eb975330923e9a980458b02f7ba98ed7c6
shingkinhei <82117981+shingkinhei@users.noreply.github.com>
2023-05-10 00:26:40 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,20 +12,14 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore();
 
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const firebaseApp = initializeApp(firebaseConfig);
+connectAuthEmulator(getAuth(app), "https://dialect-on-water-deploy.vercel.app/", {disableWarnings: true});
+
+export default firebaseApp;
 export const auth = getAuth(app);
 export { app, db };
-
-
-// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const db = getFirestore();
-// const firebaseApp = initializeApp(firebaseConfig);
-// connectAuthEmulator(getAuth(app), "https://dialect-on-water-deploy.vercel.app/", {disableWarnings: true});
-
-// export default firebaseApp;
-// export const auth = getAuth(app);
-// export { app, db };
 
 
