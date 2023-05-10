4380333f17c004a29b04d58f82ed52fe90407e7a
shingkinhei <82117981+shingkinhei@users.noreply.github.com>
2023-05-10 12:44:15 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY.replace(/\n/g，'\n'),
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN.replace(/\n/g，'\n'),
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID.replace(/\n/g，'\n'),
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET.replace(/\n/g，'\n'),
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID.replace(/\n/g，'\n'),
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID.replace(/\n/g，'\n'),
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -16,8 +16,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 const firebaseApp = initializeApp(firebaseConfig);
-
-
 
 export default firebaseApp;
 export const auth = getAuth(app);
