2aee20fb8105fdb0d3dca59fd8f197976db12abe
shingkinhei <82117981+shingkinhei@users.noreply.github.com>
2023-05-10 11:46:39 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY.replace(/\n/g，'\n'),
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN.replace(/\n/g，'\n'),
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID.replace(/\n/g，'\n'),
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET.replace(/\n/g，'\n'),
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID.replace(/\n/g，'\n'),
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID.replace(/\n/g，'\n'),
 };
 
 // Initialize Firebase
